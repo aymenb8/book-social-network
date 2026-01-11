98eb203fe849a4b3979371e19c1a059b770f4d4c
abouabid <bouabid.aymen@gmail.com>
2026-01-11 14:13:56 +0100

    rectification unit tests - test ignore on boot class

--- book-network/src/test/java/org/bouabid/book/BookSocialNetworkApplicationTests.java
+++ book-network/src/test/java/org/bouabid/book/BookSocialNetworkApplicationTests.java
@@ -2,19 +2,9 @@ package org.bouabid.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ActiveProfiles("test")
+
 class BookSocialNetworkApplicationTests {
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("jwt.secret", () -> "test-secret");
-        registry.add("spring.datasource.url", () -> "jdbc:h2:mem:testdb");
-    }
 
     @Test
     void contextLoads() {

060381806bc2e0010a642b37500130da511bb9f1
abouabid <bouabid.aymen@gmail.com>
2026-01-11 13:35:11 +0100

    rectification unit tests

--- book-network/src/test/java/org/bouabid/book/BookSocialNetworkApplicationTests.java
+++ book-network/src/test/java/org/bouabid/book/BookSocialNetworkApplicationTests.java
@@ -2,8 +2,10 @@ package org.bouabid.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookSocialNetworkApplicationTests {
 
     @Test
